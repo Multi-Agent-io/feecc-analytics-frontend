ed3e8ff99c0a2d58f504e55344af2963d3839c9c
Иван Глебов <glebov.vanya8@gmail.com>
2022-02-01 14:37:51 +0300

    Added passports page

---
+++ src/pages/Passports/Passports.js
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from 'react';
+import styles from './Passports.module.css'
+import ScanButton from "../../components/ScanButton/ScanButton";
+import Search from "../../components/Search/Search";
+import SettingsButton from "../../components/SettingsButton/SettingsButton";
+import Filters from "../../components/Filters/Filters";
+import Table from "../../components/Table/Table";
+import {doGetPassports} from "../../store/userActions";
+import {useDispatch, useSelector} from "react-redux";
+import {getAuthorizationStatus, getPassports, getPassportsNumber} from "../../store/selectors";
+import {history} from "../../store/main";
+
+
+export default function Passports(props) {
+    const pageSize = 11
+
+    let dispatch = useDispatch()
+    let authorized = useSelector(getAuthorizationStatus)
+    let [filtersValues, setFiltersValues] = useState({deviceType: [''], date: null, overwork: null, requiredFix: null})
+    let [searchValue, setSearchValue] = useState('')
+    let [page, setPage] = useState(localStorage.getItem('tablePage') || 25)
+    let passportsNumber = useSelector(getPassportsNumber)
+
+    let rows = useSelector(getPassports)?.toJS()
+
+    useEffect(() => {
+        localStorage.setItem('tablePage', page)
+        fetchPassports()
+    }, [filtersValues, page])
+
+    let fetchPassports = () => {
+        let {deviceType, date, overwork, requiredFix } = filtersValues
+        if (authorized) {
+            doGetPassports(dispatch, page, 11, searchValue, date, requiredFix, overwork)
+                .then((res) => {
+                })
+                .catch((err) => {
+                    if (err.response.status === 401)
+                        history.push('/')
+                })
+        }
+    }
+
+    let dropSettings = () => {
+        setSearchValue('')
+        setPage(1)
+    }
+
+    return (
+        <div className={styles.pageWrapper}>
+            <div className={styles.searchWrapper}>
+                <ScanButton/>
+                <Search value={searchValue} onChange={setSearchValue}/>
+                <SettingsButton/>
+            </div>
+            <div className={styles.contentWrapper}>
+                <Filters onChange={(values) => {
+                    if (page !== localStorage.getItem('tablePage') && page !== 1)
+                        setPage(1)
+                    setFiltersValues(values)
+                }} onDrop={dropSettings}/>
+                <Table
+                    onPageChange={(page) => {
+                        setPage(parseInt(page))
+                    }}
+                    rowsData={rows}
+                    startPage={page}
+                    pageSize={pageSize}
+                    passportsNumber={passportsNumber}
+                />
+            </div>
+        </div>
+    );
+}
