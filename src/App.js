e1cebea9714e03e76705ca38ed3ee40b3b619ccf
Иван Глебов <glebov.vanya8@gmail.com>
2022-02-01 14:38:43 +0300

    Added translations and authorization check

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import './i18n'
+import {useEffect} from "react";
+import {getAuthorizationStatus} from "./store/selectors";
+import {history} from "./store/main";
+import {doFetchUser} from "./store/userActions";import {useDispatch, useSelector} from "react-redux";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Passports from "./pages/Passports/Passports";
+import UnderConstruction from "./pages/UnderConstruction/UnderConstruction";
+import Login from "./pages/Login/Login";
+import Passport from "./pages/Passport/Passport";
+
+// import Schemas from "./pages/Schemas/Schemas";
+// import Employees from "./pages/Employees/Employees";
 
 function App() {
+    let authorized = useSelector(getAuthorizationStatus)
+    let dispatch = useDispatch()
+    let location = useSelector(state => state.router.location.pathname)
+    let routes = [
+        ['^/$', () => <Login/>],
+        ['^/passports', () => <Passports/>],
+        // ['^/Passports', () => <UnderConstruction/>],
+        // ['^/employees', () => <Employees/>],
+        ['^/employees', () => <UnderConstruction/>],
+        // ['^/production-schemas', () => <Schemas/>],
+        ['^/production-schemas', () => <UnderConstruction/>],
+        ['^/passport/*', () => <Passport>test</Passport>]
+
+    ]
+    let route = (path) => routes.find(r => path.match(r[0]) !== null)?.[1]?.()
+
+    useEffect(() => {
+        if(authorized && localStorage.getItem('token') !== undefined && localStorage.getItem('token') !== null) {
+            // history.push('/passports')
+            doFetchUser(dispatch)
+                .then((res) => {
+                    if (res.status === 200 && location === '/') {
+                        history.push('/passports')
+                    } else if (res.status === 401) {
+                        history.push('/')
+                        console.log('Error while fetching user. Res.status !== 200')
+                    }
+                })
+                .catch((err) => {
+                    console.log('Error while fetching user')
+                    console.log('Error code')
+                    console.log(err)
+                })
+
+        } else {
+            history.push('/')
+        }
+        if(!authorized) {
+            if(localStorage.getItem('token') !== undefined && localStorage.getItem('token') !== null)
+                doFetchUser(dispatch).then((res) => {
+                    if(res.status === 200) {
+                        history.push('/passports')
+                    } else if (res.status === 401) {
+                        history.push('/')
+                    }
+                })
+            else {
+                if(location !== '/')
+                    history.push('/')
+            }
+        }
+    })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {location !== '/' && (<Sidebar/>)}
+        {route(location)}
     </div>
   );
 }
