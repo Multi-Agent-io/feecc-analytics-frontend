ed173cf3d6f15a643f96aded80576aa1c519215b
Иван Глебов <glebov.vanya8@gmail.com>
2022-02-01 14:36:17 +0300

    Added button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import clsx from "clsx";
+import styles from './Button.module.css'
+
+export default function Button(props) {
+
+    let onClickHandler = (e) => {
+        props.onClick && props.onClick(e)
+    }
+
+    return (
+        <div>
+            <button onClick={onClickHandler} className={clsx({
+                [styles.defaultButton]: props.variant === 'default' || props.variant === undefined,
+                [styles.clearButton]: props.variant === 'clear',
+                [styles.disabled]: props.disabled
+            })}>{props.children}</button>
+        </div>
+    );
+}
