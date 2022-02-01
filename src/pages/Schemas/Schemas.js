acc645a483c892d1296c53a0570f0e6bc860342c
Иван Глебов <glebov.vanya8@gmail.com>
2022-02-01 14:33:49 +0300

    Added Schemas, Employees and UnderConstruction pages

---
+++ src/pages/Schemas/Schemas.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Schemas(props) {
+    return (
+        <div></div>
+    );
+}
---
+++ src/pages/UnderConstruction/UnderConstruction.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './UnderConstruction.module.css'
+
+export default function UnderConstruction(props) {
+    return (
+        <div className={styles.messageWrapper}>
+            Данная страница находится в разработке
+        </div>
+    );
+}
