aa7de6310684232f4a740c4ef98207f3947cb502
Иван Глебов <glebov.vanya8@gmail.com>
2022-02-03 22:43:15 +0300

    Added 'on-Enter' send

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,12 +6,16 @@ import {useTranslation} from "react-i18next";
 
 export default function Search(props) {
     let { t } = useTranslation()
+    let onKeyDown = (e) => {
+        if(e.key === "Enter")
+            props.onSearch && props.onSearch()
+    }
     return (
         <div className={styles.searchWrapper}>
             <div className={styles.searchIcon}>
                 <img src={searchIcon} alt="search icon"/>
             </div>
-            <input value={props.value || ''} onChange={(e) => props.onChange && props.onChange(e.target.value)} className={styles.searchField} placeholder={t('search.enterName')}/>
+            <input value={props.value || ''} onKeyDown={onKeyDown} onChange={(e) => props.onChange && props.onChange(e.target.value)} className={styles.searchField} placeholder={t('search.enterName')}/>
             <button onClick={() => props.onSearch && props.onSearch()} className={styles.searchButton}>{t('search.Find')}</button>
         </div>
     );
