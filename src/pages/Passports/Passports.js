9972dc4eb8a2cb766ab3bdfc3c2411ec3d7a6adb
Иван Глебов <glebov.vanya8@gmail.com>
2022-02-05 18:37:00 +0300

    Update for new filters storage

--- src/pages/Passports/Passports.js
+++ src/pages/Passports/Passports.js
@@ -7,8 +7,9 @@ import Filters from "../../components/Filters/Filters";
 import Table from "../../components/Table/Table";
 import {doGetPassports} from "../../store/userActions";
 import {useDispatch, useSelector} from "react-redux";
-import {getAuthorizationStatus, getPassports, getPassportsNumber} from "../../store/selectors";
+import {getAuthorizationStatus, getFiltersValues, getPassports, getPassportsNumber} from "../../store/selectors";
 import {history} from "../../store/main";
+import {dropStoredFilters} from "../../store/filtersActions";
 
 
 export default function Passports(props) {
@@ -16,7 +17,7 @@ export default function Passports(props) {
     let dispatch = useDispatch()
 
     let [pageSize, setPageSize] = useState(11)
-    let [filtersValues, setFiltersValues] = useState({deviceType: [''], date: null, overwork: null, requiredFix: null})
+    let [filtersValues, setFiltersValues] = useState(useSelector(getFiltersValues))
     let [searchValue, setSearchValue] = useState('')
     let [page, setPage] = useState(localStorage.getItem('tablePage') || 1)
     let [filtersDisplay, changeFiltersDisplay] = useState(true)
@@ -25,7 +26,6 @@ export default function Passports(props) {
     let authorized = useSelector(getAuthorizationStatus)
     let pages = Math.ceil(useSelector(getPassportsNumber) / pageSize)
     let rows = useSelector(getPassports)?.toJS()
-
 
     useEffect(() => {
         fetchPassports()
@@ -42,17 +42,16 @@ export default function Passports(props) {
         let {deviceType, date, overwork, requiredFix} = filtersValues
         if (authorized) {
             doGetPassports(dispatch, page, pageSize, searchValue, date, requiredFix, overwork, deviceType, sortingDirection)
-                .then((res) => {
-                    // correctPage()
-                })
+                .then((res) => {})
                 .catch((err) => {
-                    if (err.response.status === 401)
+                    if (err.response?.status === 401)
                         history.push('/')
                 })
         }
     }
 
     let dropSettings = () => {
+        dropStoredFilters(dispatch)
         setSearchValue('')
         setPage(1)
     }
