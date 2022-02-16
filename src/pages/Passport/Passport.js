57ffd3c9c8a8821622126130d9718359cdefed68
Kononov-Sergey <76957532+Kononov-Sergey@users.noreply.github.com>
2022-02-16 18:42:45 +0300

    alert in passport comp when it's in production status

--- src/pages/Passport/Passport.js
+++ src/pages/Passport/Passport.js
@@ -27,25 +27,33 @@ export default function Passport(props) {
     const { onOpenConfirm } = useContext(ModalActionsContext)
     const { changeRevision , revisionsItem } = useContext(RevisionContext)
 
+
     let [showModal, toggleModal] = useState(false)
     let [selectedStep, setSelectedStep] = useState({})
+    const [isLoading, setIsLoading] = useState('')
 
     useEffect(() => {
+        setIsLoading(false)
         doGetPassport(dispatch, location.split('/')[2])
             .then((res) => {
                 const currentPassport = res.passport;
-                // alert when not all information are available
                 if(currentPassport.status === "production"){
-                    alert("Данное изделие находится в процессе сборки.\nИнформация о изделии может быть некорректно отображена!")
+                    alert(
+                        "Данное изделие находиться в стадии разработки\nНекоторые данные могут отображаться некорректно!"
+                    )
                 }
                 currentPassport.biography.forEach((step, index) => {
                     decodeUser(dispatch, step.employee_name)
                         .then((res) => {  
+                            
                         })
                 })
+                console.log(1);
+                setIsLoading(true)
                 return currentPassport
             })
             .catch((error) => {})
+        
     }, [])
 
     const changeRevisionArrayHandler = (id, name, index, event) => {
@@ -53,7 +61,6 @@ export default function Passport(props) {
         currentBtn.classList.toggle(styles["checked-btn"]);
 
         changeRevision(id, name, index)
-        
     }
 
     
@@ -116,73 +123,74 @@ export default function Passport(props) {
     }
 
     return (
-        <div className={styles.pageWrapper}>
-            <div className={styles.passportHeaderWrapper}>
-                {passport.type !== null && passport.type !== undefined && passport.type !== '' && (<h2>{passport.type}</h2>)}
-                <div className={styles.passportNameWrapper}>
-                    <h1>{(passport.model !== null && passport.model !== '') ? passport.model : 'Без названия'}</h1>
-                    {/*<p>{revisionIds}</p>*/}
-                    <div className={styles.icons}>
-                        <img className={clsx({[styles.inactiveIcon]: !passport.overwork})} src={overworkIcon} alt="Overwork icon"/>
-                        <img className={clsx({[styles.inactiveIcon]: !passport.needFix})} src={fixRequiredIcon} alt="Fix required icon"/>
-                    </div>
+        isLoading && 
+        (<div className={styles.pageWrapper}>
+        <div className={styles.passportHeaderWrapper}>
+            {passport.type !== null && passport.type !== undefined && passport.type !== '' && (<h2>{passport.type}</h2>)}
+            <div className={styles.passportNameWrapper}>
+                <h1>{(passport.model !== null && passport.model !== '') ? passport.model : 'Без названия'}</h1>
+                {/*<p>{revisionIds}</p>*/}
+                <div className={styles.icons}>
+                    <img className={clsx({[styles.inactiveIcon]: !passport.overwork})} src={overworkIcon} alt="Overwork icon"/>
+                    <img className={clsx({[styles.inactiveIcon]: !passport.needFix})} src={fixRequiredIcon} alt="Fix required icon"/>
                 </div>
-                <h2 className={styles.passportUUId}>{passport.uuid} | {passport.internal_id}</h2>
-                <h2>{`Cерийный номер: ${passport["serial_number"] ? passport["serial_number"] : "не найдено"}`}</h2>
             </div>
-            <div className={styles.passportMainContent}>
-            {passport.biography !== null && passport.biography !== undefined && passport.biography.length > 0 ? passport.biography.map((step, index) => {
-                    return (
-                            <div key={index} className={styles.passportStepWrapper}>
-                                <div className={styles.stepContentWrapper}>
-                                    <h2>{step.name}{step.unit_name && <p>относится к <a href = {`/passport/${step.parent_unit_internal_id}/view`}>{step.unit_name}</a></p>}</h2>
-                                    <div className={styles.descriptionWrapper}>
-                                        <div className={styles.stepRowWrapper}>
-                                            <h3 className={styles.descriptionRowHeader}>Время начала:</h3>
-                                            <h3>{step.session_start_time?.split(' ')[1]}</h3>
-                                        </div><div className={styles.stepRowWrapper}>
-                                            <h3 className={styles.descriptionRowHeader}>Длительность:</h3>
-                                            {step.session_end_time ? <h3>{formatTime(step)}</h3> : <h3>Не найдено</h3>}
-                                        </div>
-                                        <div className={styles.stepRowWrapper}>
-                                            <h3 className={styles.descriptionRowHeader}>Исполнитель:</h3>
-                                            <h3>{employees[step.employee_name]}</h3>
-                                        </div>
-                                        <div className={styles.stepRowWrapper}>
-                                            <h3 className={styles.descriptionRowHeader}>Дата завершения:</h3>
-                                            {step.session_end_time ? <h3>{extractDate(step.session_end_time)}</h3> : <h3>Не найдено</h3>}
-                                        </div>
+            <h2 className={styles.passportUUId}>{passport.uuid} | {passport.internal_id}</h2>
+            <h2>{`Cерийный номер: ${passport["serial_number"] ? passport["serial_number"] : "не найдено"}`}</h2>
+        </div>
+        <div className={styles.passportMainContent}>
+        {passport.biography !== null && passport.biography !== undefined && passport.biography.length > 0 ? passport.biography.map((step, index) => {
+                return (
+                        <div key={index} className={styles.passportStepWrapper}>
+                            <div className={styles.stepContentWrapper}>
+                                <h2>{step.name}{step.unit_name && <p>относится к <a href = {`/passport/${step.parent_unit_internal_id}/view`}>{step.unit_name}</a></p>}</h2>
+                                <div className={styles.descriptionWrapper}>
+                                    <div className={styles.stepRowWrapper}>
+                                        <h3 className={styles.descriptionRowHeader}>Время начала:</h3>
+                                        <h3>{step.session_start_time?.split(' ')[1]}</h3>
+                                    </div><div className={styles.stepRowWrapper}>
+                                        <h3 className={styles.descriptionRowHeader}>Длительность:</h3>
+                                        {step.session_end_time ? <h3>{formatTime(step)}</h3> : <h3>Не найдено</h3>}
+                                    </div>
+                                    <div className={styles.stepRowWrapper}>
+                                        <h3 className={styles.descriptionRowHeader}>Исполнитель:</h3>
+                                        <h3>{employees[step.employee_name]}</h3>
+                                    </div>
+                                    <div className={styles.stepRowWrapper}>
+                                        <h3 className={styles.descriptionRowHeader}>Дата завершения:</h3>
+                                        {step.session_end_time ? <h3>{extractDate(step.session_end_time)}</h3> : <h3>Не найдено</h3>}
                                     </div>
                                 </div>
-                                <div className={clsx(styles.stepVideoPreview, {[styles.notFound]: step.video_hashes === null})}
-                                     onClick={() => {
-                                         if(step.video_hashes !== null) {
-                                             setSelectedStep(step)
-                                             toggleModal(true)
-                                         }
-                                     }}
-                                >
-                                    <h2>{step.video_hashes !== null ? "Превью видеозаписи" : "Запись недоступна"}</h2>
-                                </div>
-                                {editModeIsOn &&
-                                    <div className={styles.configurationButtonsWrapper}>
-                                        <Button
-                                            onClick = {changeRevisionArrayHandler.bind(null, step.id, step.name, index)}
-                                            variant = {step.unit_name === null ? "default" : "clear"}
-                                            disabled = {step.unit_name === null ? false : true}
-                                        >
-                                        Нужна доработка
-                                        </Button>
-                                    </div>
-                                }
-                                
                             </div>
-                    )}
-            ) : (
-                <h1 className={styles.noRequiredInformation}>{t('passport.noRequiredInformation')}</h1>
-            )}
-            {editModeIsOn && <Button onClick ={onOpenConfirm}>Отправить на добработку</Button>}
-            </div>
+                            <div className={clsx(styles.stepVideoPreview, {[styles.notFound]: step.video_hashes === null})}
+                                 onClick={() => {
+                                     if(step.video_hashes !== null) {
+                                         setSelectedStep(step)
+                                         toggleModal(true)
+                                     }
+                                 }}
+                            >
+                                <h2>{step.video_hashes !== null ? "Превью видеозаписи" : "Запись недоступна"}</h2>
+                            </div>
+                            {editModeIsOn &&
+                                <div className={styles.configurationButtonsWrapper}>
+                                    <Button
+                                        onClick = {changeRevisionArrayHandler.bind(null, step.id, step.name, index)}
+                                        variant = {step.unit_name === null ? "default" : "clear"}
+                                        disabled = {step.unit_name === null ? false : true}
+                                    >
+                                    Нужна доработка
+                                    </Button>
+                                </div>
+                            }
+                            
+                        </div>
+                )}
+        ) : (
+            <h1 className={styles.noRequiredInformation}>{t('passport.noRequiredInformation')}</h1>
+        )}
+        {editModeIsOn && <Button onClick ={onOpenConfirm}>Отправить на добработку</Button>}
+        </div>
 
             {showModal && (
                 <div className={styles.modalWrapper}>
@@ -198,6 +206,6 @@ export default function Passport(props) {
                 </div>
             )}
 
-        </div>
+        </div>)
     );
 }
