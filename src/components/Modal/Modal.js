39ae79cc640da2deda15da98ae0f12ba7d328003
Сергей Кононов <kobo220376@gmail.com>
2022-02-20 23:43:50 +0300

    little fix

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,13 @@
 import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
 import { useContext} from "react/cjs/react.development";
-import ModalActionsContext from "../../store/modal-context";
+import ModalActionsContext  from "../../store/modal-context";
 
 function Backdrop(props) {
-
+  
   const { onClose } = useContext(ModalActionsContext);
-  return <div onClick={() => {onClose()}} className={classes.backdrop}></div>;
+
+  return <div onClick={onClose} className={classes.backdrop}></div>;
 }
 
 function ModalOverlay(props) {
