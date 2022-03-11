06bf8b369dc653036d3f2118ae045ec6dd7b72f6
Иван Глебов <glebov.vanya8@gmail.com>
2022-03-11 20:12:52 +0300

    Added translations and finished SelectMultiple component

--- src/components/SelectMultiple/SelectMultiple.js
+++ src/components/SelectMultiple/SelectMultiple.js
@@ -1,15 +1,16 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import clsx from "clsx";
-import styles from './Select.module.css'
+import styles from './SelectMultiple.module.css'
 import Checkbox from "../Checkbox/Checkbox";
 import arrowDownIcon from '../../assets/arrow_down.svg'
+import {useTranslation} from "react-i18next";
 
-export default function Select(props) {
-
+export default function SelectMultiple(props) {
 
     const [checkboxes, setCheckboxes] = useState([])
     let [selectStatus, toggleSelect] = useState(false)
     let [selectedElements, changeSelection] = useState('Выберите из списка')
+    let {t} = useTranslation()
 
     const resetSelection = () => {
         let arr = [...checkboxes]
@@ -29,10 +30,16 @@ export default function Select(props) {
 
     useEffect(() => {
         setCheckboxes(props.options)
-    }, [])
+    }, [props.options])
 
     useEffect(() => {
-        console.log(checkboxes)
+        let res = ''
+        checkboxes.filter((v) => v.state === true).forEach((item, index, arr) => {
+            res += item.name
+            if(index !== arr.length - 1)
+                res += ','
+        })
+        props.onChange && props.onChange(res)
     }, [checkboxes])
 
     return (
@@ -46,7 +53,7 @@ export default function Select(props) {
                 </div>
                 <div className={clsx(styles.checkboxesWrapper, {[styles.hidden]: !selectStatus})}>
                     <div className={styles.dropButtonWrapper}>
-                        <h2 onClick={resetSelection} className={styles.dropButton}>Снять все выделения</h2>
+                        <h2 onClick={resetSelection} className={styles.dropButton}>{t('filters.selects.removeSelections')}</h2>
                     </div>
                     {checkboxes.map((item, index) => {
                         return (
