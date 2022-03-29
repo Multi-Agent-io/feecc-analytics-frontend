7a4d52e852a0c16ae387f3c11d9d915404f88fba
Иван Глебов <glebov.vanya8@gmail.com>
2022-03-29 11:53:56 +0300

    New imports for components

---
+++ src/components/index.js
@@ -0,0 +1,35 @@
+import Button from "./Button/Button";
+import ButtonBack from "./ButtonBack/ButtonBack";
+import Checkbox from "./Checkbox/Checkbox";
+import DatePicker from "./DatePicker/DatePicker"
+import Filters from "./Filters/Filters";
+import Input from "./Input/Input";
+import Modal from "./Modal/Modal";
+import PrintButton from "./PrintButton/PrintButton"
+import ScanButton from "./ScanButton/ScanButton";
+import Search from './Search/Search'
+import SearchProtocol from "./SearchProtocol/SearchProtocol"
+import Select from "./Select/Select";
+import SelectMultiple from "./SelectMultiple/SelectMultiple";
+import SettingsButton from "./SettingsButton/SettingsButton";
+import Sidebar from "./Sidebar/Sidebar";
+import Table from "./Table/Table";
+
+export {
+  Button,
+  ButtonBack,
+  Checkbox,
+  DatePicker,
+  Filters,
+  Input,
+  Modal,
+  PrintButton,
+  ScanButton,
+  Search,
+  SearchProtocol,
+  Select,
+  SelectMultiple,
+  SettingsButton,
+  Sidebar,
+  Table
+}
