2464c36771a2de594dc64c206963bec90c0d5fce
Иван Глебов <glebov.vanya8@gmail.com>
2022-05-16 17:14:21 +0300

    Tag shadows update

--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import styles from './Tag.module.css'
+import React from "react";
+import styles from "./Tag.module.css";
 
 function Tag(props) {
-  return (<div className={ `
-      ${ styles.tagWrapper }
-      ${ props.style === 'warning' && styles.warningTag }
-      ${ props.hover && styles.tagHover }
-      ` }>{ props.children }</div>);
+  return (
+    <div
+      className={`
+      ${styles.tagWrapper}
+      ${props.style === "warning" && styles.warningTag}
+      ${props.hover && styles.tagHover}
+      ${props.shadow && styles.addShadow}
+      `}
+    >
+      {props.children}
+    </div>
+  );
 }
 
 export default Tag;
