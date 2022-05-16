96b4aa7c513cf50047331053962ed5ea7d418032
Иван Глебов <glebov.vanya8@gmail.com>
2022-05-16 17:23:29 +0300

    Code reformatted using Prettier

--- src/store/main.js
+++ src/store/main.js
@@ -1,27 +1,26 @@
-import {applyMiddleware, combineReducers, compose, createStore} from "redux";
-import {composeWithDevTools} from "redux-devtools-extension";
-import {userReducer, userInitialState} from "./userReducer";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { userReducer, userInitialState } from "./userReducer";
 
-import { createBrowserHistory } from 'history'
-import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { createBrowserHistory } from "history";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 
-
-export const history = createBrowserHistory()
+export const history = createBrowserHistory();
 
 // let middleware = [
 //     routerMiddleware(history)
 // ]
 
 export const store = createStore(
-    combineReducers({
-        user: userReducer,
-        router: connectRouter(history)
-    }),
-    {
-        user: userInitialState
-    },
-    composeWithDevTools()
+  combineReducers({
+    user: userReducer,
+    router: connectRouter(history),
+  }),
+  {
+    user: userInitialState,
+  },
+  composeWithDevTools()
   // compose(
-    //     applyMiddleware(routerMiddleware(history))
-    // )
-)
+  //     applyMiddleware(routerMiddleware(history))
+  // )
+);
