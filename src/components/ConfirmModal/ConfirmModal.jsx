7d4b6b459ee81504aee168b33a7ac61f75af8a15
Иван Глебов <glebov.vanya8@gmail.com>
2022-06-14 16:11:00 +0300

    Confirm modal component code style updated

---
+++ src/components/ConfirmModal/ConfirmModal.jsx
@@ -0,0 +1,75 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useSnackbar } from 'notistack';
+import Button from '../Button/Button';
+
+import Modal from '../Modal/Modal';
+import { history } from '../../store/main';
+
+import ModalActionsContext from '../../store/modal-context';
+import RevisionContext from '../../store/revision-context';
+
+import classes from './ConfirmModal.module.css';
+
+import conf from '../../config.json';
+
+function ConfirmModal() {
+  const { modalConfirmStatus, onClose } = useContext(ModalActionsContext);
+  const { revisionsItem } = useContext(RevisionContext);
+  const [nameRevision, setNameRevision] = useState([]);
+  const [idsRevision, setIdsRevision] = useState([]);
+
+  const { enqueueSnackbar } = useSnackbar();
+  const addNotification = (message, variant) => enqueueSnackbar(message, { variant });
+
+  const pushToServerHandler = () => {
+    const internalId = window.location.pathname.split('/')[2];
+    const desiredArray = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const id of idsRevision) {
+      if (id) desiredArray.push(id);
+    }
+
+    const url = `${conf.base_url}/api/v1/passports/${internalId}/revision`;
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: JSON.stringify(desiredArray),
+    })
+      .then(() => {
+        onClose();
+        addNotification('Этапы отправлены на доработку', 'success');
+        history.push('/tcd');
+      });
+  };
+
+  useEffect(() => {
+    // const tempRevisionsItem = { ...revisionsItem };
+    setNameRevision(revisionsItem.filter((item) => Object.values(item)[0] || 'Имени нет'));
+    setIdsRevision(revisionsItem.map((item) => Object.keys(item)[0] || 'Имени нет'));
+  }, [revisionsItem]);
+
+  if (modalConfirmStatus) {
+    return (
+      <Modal>
+        <section className={classes.contentUl}>
+          <h1>Выбранные стадии на доработку:</h1>
+          <ol>
+            {nameRevision.map((item) => (item ? <li key={item.name}>{item}</li> : null))}
+          </ol>
+          <div className={classes['btn-section']}>
+            <Button onClick={onClose}>Отмена</Button>
+            <Button variant="clear" onClick={pushToServerHandler}>Отправить на доработку</Button>
+          </div>
+        </section>
+      </Modal>
+    );
+  }
+
+  return (<div />);
+}
+
+export default ConfirmModal;
