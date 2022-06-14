326453f53e42c98138457a3dde51d0f06d6b0d15
Иван Глебов <glebov.vanya8@gmail.com>
2022-06-14 16:19:29 +0300

    RemoveProtocolModal code style update

---
+++ src/components/RemoveProtocolModal/RemoveProtocolModal.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+
+// eslint-disable-next-line import/no-cycle
+import { Modal, Button } from '..';
+import { history } from '../../store/main';
+import ModalActionsContext from '../../store/modal-context';
+
+import classes from './RemoveProtocolModal.module.css';
+
+import { doRemoveProtocol } from '../../store/userActions';
+
+function RemoveProtocolModal() {
+  const { modalDeleteStatus, onClose } = useContext(ModalActionsContext);
+
+  const pushToServerHandler = () => {
+    const internalId = window.location.pathname.split('/')[3];
+    doRemoveProtocol(internalId);
+    onClose();
+    history.push('/tcd');
+  };
+
+  return (
+    modalDeleteStatus && (
+      <Modal>
+        <section className={classes.contentUl}>
+          <h1>Удалить протокол?</h1>
+          <div className={classes["btn-section"]}>
+            <Button onClick={onClose}>Отмена</Button>
+            <Button variant="delete" onClick={pushToServerHandler}>
+              Удалить
+            </Button>
+          </div>
+        </section>
+      </Modal>
+    )
+  );
+}
+
+export default RemoveProtocolModal;
