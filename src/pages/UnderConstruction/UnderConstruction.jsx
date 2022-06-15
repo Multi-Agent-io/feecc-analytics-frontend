b60d5137fc16d948380ab648ae6d2a7d92797a1a
Иван Глебов <glebov.vanya8@gmail.com>
2022-06-15 15:11:12 +0300

    Under construction page code style updated

---
+++ src/pages/UnderConstruction/UnderConstruction.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './UnderConstruction.module.css';
+
+export default function UnderConstruction() {
+  return (
+    <div className={styles.messageWrapper}>
+      Данная страница находится в разработке
+    </div>
+  );
+}
