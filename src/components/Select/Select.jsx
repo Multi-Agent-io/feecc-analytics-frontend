22dd302c4a3ab7b11b46a3afeddaa158c391455c
Иван Глебов <glebov.vanya8@gmail.com>
2022-10-29 23:50:34 +0300

    Select component checkboxes logic update

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -11,21 +11,20 @@ import arrowDownIcon from '../../assets/arrow_down.svg';
 export default function Select({ options, onChange }) {
   const [checkboxes, setCheckboxes] = useState([]);
   const [selectStatus, toggleSelect] = useState(false);
-  const [selectedElement, setSelectedElement] = useState(options[0].name);
+  const [selectedElement, setSelectedElement] = useState(options[0].value);
   const { t } = useTranslation();
 
   const updateCheckbox = (index) => {
-    let arr = [...checkboxes];
-    arr = arr.map((item, innerIndex) => {
-      const element = { ...item };
-      if (index === innerIndex) {
-        element.state = true;
-        setSelectedElement(item.name);
-      } else element.state = false;
-      return element;
+    const arr = [...checkboxes];
+    arr.forEach((_e, innerIndex) => {
+      arr[innerIndex].state = false;
     });
+
+    const el = arr[index];
+    arr[index] = { ...el, state: true };
+    setSelectedElement(el.name);
     setCheckboxes(arr);
-    if (onChange !== undefined) {
+    if (onChange) {
       onChange({ name: checkboxes[index].name, value: checkboxes[index].value });
     }
   };
